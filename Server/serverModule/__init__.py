c0ceca72e07a4a551f288dbf4ae069c94a9d5bc2
Sang Hyeok Han <contea95@naver.com>
2021-10-31 14:51:13 +0900

    fix: fix db.sqlite and foldering

--- Server/serverModule/__init__.py
+++ Server/serverModule/__init__.py
@@ -1,9 +1,7 @@
-
 from flask import Flask
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 import os
-
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
@@ -18,7 +16,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbfile
 # 로직이 끝날 때 DB에 반영
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 # 수정사항 Track 안함
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 # DB Secret key
 app.config['SECRET_KEY'] = 'rkskekfkakqktk'
 
