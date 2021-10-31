10a6afd700c9a2f797128ea1c591eb4759c19ca2
Sang Hyeok Han <contea95@naver.com>
2021-10-31 16:45:48 +0900

    feat: ADD Flask test page

--- Server/app.py
+++ Server/app.py
@@ -1,15 +1,19 @@
-from flask import redirect
+from flask import redirect, render_template, request
 from serverModule import app, db
 from serverModule.model import Url
+import validators
 
 
 @app.route('/')
-def hello():
-    return f'{"hello"}'
+def index():
+    return render_template("index.html")
 
 
 @app.route('/encode/<input_url>')
 def encode(input_url):
+    if str(input_url).startswith('http://') == False or str(input_url).startswith('https://') == False:
+        print("malfunction")
+        return render_template('404.html'), 404
     count = Url.query.filter_by(origin_url=input_url).first()
     if count is None:
         # DB 내 단축 URL이 없을 경우
@@ -31,6 +35,35 @@ def redirection(input_code):
     url = Url.query.filter_by(id=input_code).first()
     # return f'{url.origin_url}'
     return redirect("https://" + str(url.origin_url), code=302)
+
+
+@app.route('/result', methods=['POST', 'GET'])
+def result():
+    if request.method == 'POST':
+        # HTML 폼에서 전달받은 url
+        url = request.form['url']
+        input_url = str(url)
+
+        if str(input_url).startswith('http://') or str(input_url).startswith('https://'):
+            print("start http or https")
+    count = Url.query.filter_by(origin_url=input_url).first()
+    if count is None:
+        # DB 내 단축 URL이 없을 경우
+        url = Url(origin_url=input_url)
+        db.session.add(url)
+        db.session.commit()
+        id_num = url.search_id_by_origin_url(origin_url=input_url)
+        print("new url Update!")
+        # return f'{id_num.id}'
+        return render_template("result.html", url=id_num.id)
+    else:
+        # DB 내 단축 URL이 있을 경우
+        url = Url.query.filter_by(origin_url=input_url).first()
+        print("URL READ IN DB")
+        # return f'{url.id}'
+        return render_template("result.html", url=url.id)
+
+        # return render_template("result.html", url=url)
 
 
 if __name__ == "__main__":
